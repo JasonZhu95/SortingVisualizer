8575e8c91e71c525eb9cd4cdf1350a3d2f0a620d
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-08-26 04:22:30 -0700

    Added Quick Sort

--- Sorting/src/sort_type.cpp
+++ Sorting/src/sort_type.cpp
@@ -19,7 +19,7 @@ void bubbleSort()
 			if (arrayElements[j] > arrayElements[j + 1])
 			{
 				swap(&arrayElements[j], &arrayElements[j + 1]);
-				bool = true;
+				bool swapped = true;
 				Sleep(sTime);
 				draw(j, j + 1);
 			}
@@ -44,7 +44,6 @@ void selectionSort()
 			if (arrayElements[j] < arrayElements[min_index])
 			{
 				min_index = j;
-
 			}
 		}
 		swap(&arrayElements[min_index], &arrayElements[i]);
@@ -67,9 +66,42 @@ void insertionSort()
 	}
 }
 
-void quickSort()
+int partition(int l, int r)
 {
+	int pivot = arrayElements[r];
+	int i = l - 1;
+	for (int j = i; j <= r - 1; j++)
+	{
+		draw(i, j);
+		if (arrayElements[i] < pivot)
+		{
+			i++;
+			swap(&arrayElements[i], &arrayElements[j]);
+		}
+		Sleep(sTime);
+		draw(i, j);
+	}
+	draw(i + 1, r);
+	swap(&arrayElements[i + 1], &arrayElements[r]);
+	Sleep(sTime);
+	draw(i + 1, r);
+	return (i + 1);
+}
 
+void quickSort(int l, int r)
+{
+	if (l < r)
+	{
+		int p = partition(l, r);
+		Sleep(sTime);
+		draw(-1, 1);
+		quickSort(l, p - 1);
+		quickSort(p + 1, r);
+	}
+	else if (l == r) {
+		isSorted[l] = true;
+		draw(-1, -1);
+	}
 }
 
 void mergeSort()
@@ -77,17 +109,17 @@ void mergeSort()
 
 }
 
-void countingSort()
-{
-
-}
-
-void radixSort()
-{
-
-}
-
-void bucketSort()
-{
-
-}
+//void countingSort()
+//{
+//
+//}
+//
+//void radixSort()
+//{
+//
+//}
+//
+//void bucketSort()
+//{
+//
+//}
