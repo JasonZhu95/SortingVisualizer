3bd240cb3f2f2efb8dc703f7a4f4728dff890a9e
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-08-25 22:48:26 -0700

    Added two types of sort

--- Sorting/src/sort_type.cpp
+++ Sorting/src/sort_type.cpp
@@ -1,27 +1,70 @@
 #include "sort_type.h"
 
+
+void swap(float* a, float* b) {
+	float t = *a;
+	*a = *b;
+	*b = t;
+}
+
 void bubbleSort()
 {
-	for (int i = 0; i < numElements; i++) {
-		for (int j = 0; j < numElements - i - 1; j++) {
+	bool swapped;
+	for (int i = 0; i < numElements; i++)
+	{
+		swapped = false;
+		for (int j = 0; j < numElements - i - 1; j++)
+		{
 			draw(j, j + 1);
 			if (arrayElements[j] > arrayElements[j + 1])
+			{
 				swap(&arrayElements[j], &arrayElements[j + 1]);
-			Sleep(sTime);
-			draw(j, j + 1);
+				bool = true;
+				Sleep(sTime);
+				draw(j, j + 1);
+			}
 		}
 		isSorted[numElements - i - 1] = true;
 		draw(-1, -1);
+		if (swapped = false)
+		{
+			break;
+		}
 	}
 }
+
 void selectionSort()
 {
+	int i, j, min_index;
+	for (i = 0; i < numElements; i++)
+	{
+		min_index = i;
+		for (j = 0; j < numElements; j++)
+		{
+			if (arrayElements[j] < arrayElements[min_index])
+			{
+				min_index = j;
 
+			}
+		}
+		swap(&arrayElements[min_index], &arrayElements[i]);
+	}
 }
 
 void insertionSort()
 {
-
+	int i, j, key;
+	for (int i = 1; i < numElements; i++)
+	{
+		key = arrayElements[i];
+		j = i - 1;
+		while (j >= 0 && arrayElements[i] > key)
+		{
+			arrayElements[j + 1] = arrayElements[j];
+			j = j - 1;
+		}
+		arrayElements[j + 1] = key;
+	}
 }
 
 void quickSort()
@@ -47,10 +90,4 @@ void radixSort()
 void bucketSort()
 {
 
-}
-
-void swap(float* a, float* b) {
-	float t = *a;
-	*a = *b;
-	*b = t;
 }
